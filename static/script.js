f301190671db3da0ec7fdf7f51e9e590691db8c5
Ameer Hamza <120208772+ameerhamza7763@users.noreply.github.com>
2024-04-07 13:30:53 +0530

    ecom

---
+++ static/script.js
@@ -0,0 +1,28 @@
+console.log('This is working');
+
+    if(localStorage.getItem('cart')==null){
+        var cart = {};
+    }
+    else{
+        cart = JSON.parse(localStorage.getItem('cart'));
+    }
+
+    $(document).on('click','.atc',function(){
+        console.log("The add to cart button is clicked");
+        var item_id = this.id.toString();
+        console.log(item_id);
+
+        if(cart[item_id]!=undefined){
+            cart[item_id] = cart[item_id] + 1;
+        }
+        else{
+            cart[item_id] = 1;
+        }
+        console.log(cart);
+        localStorage.setItem('cart',JSON.stringify(cart));
+        document.getElementById("cart").innerHTML = "Cart("+ Object.keys(cart).length +")";
+});
+
+    function myFunction() {
+  document.getElementById("cart").innerHTML = "<h5>This is your Cart</h5>";
+}
